446dac292455b2b74753d9440f99f174d531ff58
NathanB512 <nathan.brown909@outlook.com>
2026-09-16 14:54:05 +0100

    Basic LLM with manual user input

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from ollama import chat
 # The chat functions enables a conversation with an llm
 response = chat(
     model="qwen3:1.7b",
-        # Models do not have memory, rather we send in a list of messages rto provide context
+        # Models do not have memory, rather we send in a list of messages to provide context
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
+        # The user represents the person asking a question
         {"role": "user", "content": "What is the capital of France?"}
     ]
 )
