c0925e7ef6809b24793a2f56f2182bc8dc78ebcc
NathanB512 <nathan.brown909@outlook.com>
2026-09-16 12:00:22 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Ollama is a Python package that allows you to interface with Ollama models
+from ollama import chat
+
+# The chat functions enables a conversation with an llm
+response = chat(
+    model="qwen3:1.7b",
+        # Models do not have memory, rather we send in a list of messages rto provide context
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is the capital of France?"}
+    ]
+)
+
+print(response.message.content)
